e8d27b074b1f2b52abc3654a407e334ed5d49c04
Manish Maharjan <maharjanm96@gmail.com>
2024-09-11 13:22:39 +0545

    Admin Login/Signup

--- app/api/admin/adminlogin/route.ts
+++ app/api/admin/adminlogin/route.ts
@@ -15,46 +15,55 @@ export const POST = async (request: NextRequest) => {
     await connectMongo();
     const existingUser = await Users.findOne({
       email: lowerCaseEmail,
-      userType: "admin",
+      userRole: "ADMIN",
     });
 
     if (existingUser && existingUser.isVerified) {
       const match = await bcrypt.compare(password, existingUser.password);
-      // check if password is correct
+
       if (!match) {
-        return new NextResponse(JSON.stringify("Incorrect Credentials"), {
-          status: 200,
-        });
+        return NextResponse.json(
+          { message: "Incorrect Credentials" },
+          {
+            status: 200,
+          }
+        );
       }
 
       const tokenData = {
         id: existingUser._id,
         email: existingUser.email,
-        userType: existingUser.userType,
+        userRole: existingUser.userRole,
         joinedDate: existingUser.joinedDate,
       };
 
-      const token = jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+      const token = jwt.sign(tokenData, process.env.JWT_SECRET_KEY!, {
         expiresIn: "1d",
       });
 
-      const response = new NextResponse(JSON.stringify(existingUser.userType), {
+      const response = NextResponse.json(existingUser.userRole, {
         status: 201,
       });
 
       response.cookies.set("token", token, { httpOnly: true });
-      console.log("Login success");
+      console.log("Login success", tokenData);
       return response;
     } else {
       console.log("User not found");
-      return new NextResponse(JSON.stringify("Incorrect Credentials"), {
-        status: 200,
-      });
+      return NextResponse.json(
+        { message: "Incorrect Credentials" },
+        {
+          status: 200,
+        }
+      );
     }
   } catch (error) {
     console.log(error);
-    return new NextResponse(JSON.stringify({ error: "Login Failed" }), {
-      status: 400,
-    });
+    return NextResponse.json(
+      { error: "Login Failed" },
+      {
+        status: 400,
+      }
+    );
   }
 };
--- app/api/admin/adminsignup/route.ts
+++ app/api/admin/adminsignup/route.ts
@@ -9,78 +9,70 @@ export const POST = async (request: NextRequest) => {
   console.log("Running POST request: Add Admin");
 
   try {
-    const { email, password, userType } = await request.json();
+    const { name, email, password, userRole } = await request.json();
     const lowerCaseEmail = email.toLowerCase();
+
+    if (userRole !== "ADMIN") {
+      return NextResponse.json({ error: "Invalid user role" }, { status: 400 });
+    }
+
     await connectMongo();
-    const existingUser = await UserModel.findOne({
-      email: lowerCaseEmail,
-    });
+    const existingUser = await UserModel.findOne({ email: lowerCaseEmail });
 
     if (existingUser) {
       if (existingUser.isVerified) {
-        console.log("User Already Exist");
-        return new NextResponse(JSON.stringify("User Already Exist"), {
-          status: 200,
-        });
+        console.log("User Already Exists");
+        return NextResponse.json(
+          { message: "User Already Exists" },
+          { status: 409 }
+        );
       } else {
-        // Update the existing user's information
         try {
           const hashedPassword = await bcrypt.hash(password, 10);
           await UserModel.updateOne(
             { email: lowerCaseEmail },
-            {
-              $set: {
-                password: hashedPassword,
-              },
-            }
+            { $set: { password: hashedPassword } }
           );
 
-          return new NextResponse(JSON.stringify("Signup Successfull !"), {
-            status: 201,
-          });
+          return NextResponse.json(
+            { message: "Signup Successful!" },
+            { status: 201 }
+          );
         } catch (error) {
-          return new NextResponse(
-            JSON.stringify({ error: "User update failed" }),
+          return NextResponse.json(
+            { error: "User update failed" },
             { status: 500 }
           );
         }
       }
     } else {
-      let hashedPassword = undefined;
-
       try {
-        hashedPassword = await bcrypt.hash(password, 10);
-      } catch (error) {
-        return new NextResponse(JSON.stringify({ error: "hashed failed" }), {
-          status: 400,
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new UserModel({
+          name: name,
+          email: lowerCaseEmail,
+          password: hashedPassword,
+          userRole: "ADMIN",
+          joinedDate: Date.now(),
         });
-      }
-      // Create a new user document
-      const newUser = new UserModel({
-        email: lowerCaseEmail,
-        password: hashedPassword,
-        userType,
-        isVerified: true,
-        joinedDate: Date.now(),
-      });
 
-      try {
-        const savedUser = await newUser.save();
-        console.log("New Admin added");
+        await newUser.save();
 
-        return new NextResponse(JSON.stringify("Signup Successfull !"), {
-          status: 201,
-        });
-      } catch (error) {
-        return new NextResponse(
-          JSON.stringify({ error: "User creation failed" }),
+        return NextResponse.json(
+          { message: "Signup Successful!" },
+          { status: 201 }
+        );
+      } catch (error: any) {
+        return NextResponse.json(
+          { error: "User creation failed" },
           { status: 500 }
         );
       }
     }
   } catch (error) {
-    return new NextResponse(JSON.stringify({ error: "Invalid request body" }), {
-      status: 400,
-    });
+    return NextResponse.json(
+      { error: "Invalid request body" },
+      { status: 400 }
+    );
   }
 };
