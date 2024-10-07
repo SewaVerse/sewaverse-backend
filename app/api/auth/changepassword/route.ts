90d84681062a39e72c01b5579c6a33ff8a255954
Manish Maharjan <maharjanm96@gmail.com>
2024-10-07 13:43:06 +0545

    Change password updated

--- app/api/auth/changepassword/route.ts
+++ app/api/auth/changepassword/route.ts
@@ -1,16 +1,24 @@
 import { NextResponse, NextRequest } from "next/server";
-import { hashPassword } from "@/lib/hashpassword";
 import connectMongo from "@/lib/connectMongo";
 import bcrypt from "bcryptjs";
-import { getUserById } from "@/data/user";
 import { currentUser } from "@/lib/auth";
-import { auth } from "@/auth";
 import UserModel from "@/models/Users/User";
+
+const hashPassword = async (password: string) => {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+};
 
 export const POST = async (request: NextRequest) => {
   console.log("Running POST: Change Password");
   const user = await currentUser();
 
+  if (!user || !user.id) {
+    return NextResponse.json(
+      { message: "No authenticated user found" },
+      { status: 404 }
+    );
+  }
   try {
     const data = await request.json();
     const { oldPassword, newPassword } = data;
@@ -18,7 +26,7 @@ export const POST = async (request: NextRequest) => {
     await connectMongo();
     console.log("MongoDB Connected");
 
-    const existingUser = await UserModel.findOne({ _id: user?._id });
+    const existingUser = await UserModel.findOne({ _id: user?.id });
     if (!existingUser)
       return NextResponse.json({ message: "No user found" }, { status: 404 });
 
@@ -39,10 +47,10 @@ export const POST = async (request: NextRequest) => {
 
     return NextResponse.json(
       { message: "Password changed successfully!" },
-      { status: 201 }
+      { status: 200 }
     );
   } catch (error: any) {
-    console.error("Something went wrong", error);
+    console.error("Failed to change password", error);
     return NextResponse.json(
       { message: "Something went wrong", error },
       { status: 500 }
