7407967c4ecd47fa722e455773d9dc6a18a31fb2
Manish Maharjan <maharjanm96@gmail.com>
2024-10-20 12:23:48 +0545

    User Update

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,47 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import { currentUser, currentRole } from "@/lib/auth";
 import connectMongo from "@/lib/connectMongo";
-import UserModel from "@/models/Users/User";
 import ServiceProviderModel from "@/models//Users/ServiceProvider";
 import CompanyModel from "@/models/Users/Company";
-
-export const GET = async () => {
-  console.log("Running GET Request:Get User Details");
-  try {
-    await connectMongo();
-
-    const role = await currentRole();
-    const user = await currentUser();
-
-    if (!user || !role) {
-      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-    }
-
-    if (role === "USER") {
-      const User = await UserModel.findById(user.id).select("-password");
-      return NextResponse.json(User, { status: 200 });
-    }
-
-    if (role === "SERVICE_PROVIDER") {
-      const serviceProvider = await ServiceProviderModel.findOne({
-        linkedUserId: user.id,
-      }).select("-password");
-      return NextResponse.json(serviceProvider, { status: 200 });
-    }
-
-    if (role === "COMPANY") {
-      const company = await CompanyModel.findOne({
-        linkedUserId: user.id,
-      }).select("-password");
-      return NextResponse.json(company, { status: 200 });
-    }
-
-    return NextResponse.json({ message: "Not Authorized" }, { status: 401 });
-  } catch (error) {
-    console.error("Error fetching user info:", error);
-    return NextResponse.json({ message: "Server error" }, { status: 500 });
-  }
-};
+import UserProfile from "@/models/Users/UserProfile";
+import UserModel from "@/models/Users/User";
 
 export const POST = async (request: NextRequest) => {
   console.log("Running POST Request: Update User");
@@ -49,28 +12,36 @@ export const POST = async (request: NextRequest) => {
   const user = await currentUser();
   const role = await currentRole();
 
+  console.log(user, role);
+
   if (!user || !role) {
     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
   }
 
   try {
     const data = await request.json();
-    console.log(data);
+    console.log(data.id);
     await connectMongo();
 
+    const existingUser = await UserModel.findOne({ _id: data.id });
+    if (!existingUser) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
+
     if (role === "SERVICE_PROVIDER") {
-      const existingDoc = await ServiceProviderModel.findOne({
+      const existingServiceProvider = await ServiceProviderModel.findOne({
         linkedUserId: data.id,
       });
-      console.log(existingDoc);
-      if (existingDoc) {
-        await existingDoc.updateOne({
+      if (existingServiceProvider) {
+        await existingServiceProvider.updateOne({
           $set: {
             ...data,
-            updatedDate: Date.now(),
           },
         });
-        return NextResponse.json({ message: "User Updated" }, { status: 201 });
+        return NextResponse.json(
+          { message: "Service Provider Updated" },
+          { status: 201 }
+        );
       } else {
         return NextResponse.json(
           { message: "Service Provider not found" },
@@ -78,18 +49,22 @@ export const POST = async (request: NextRequest) => {
         );
       }
     } else if (role === "USER") {
-      const existingUser = await UserModel.findOne({ _id: data.id });
-      if (existingUser) {
-        await existingUser.updateOne({
-          $set: {
-            ...data,
-            updatedDate: Date.now(),
-          },
-        });
-        return NextResponse.json({ message: "User Updated" }, { status: 201 });
+      const existingUserProfile = await UserProfile.findOne({
+        linkedUserId: data.id,
+      });
+      if (existingUserProfile) {
+        await existingUserProfile.updateOne(
+          { linkedUserId: data._id },
+          { $set: data }
+        );
+
+        return NextResponse.json(
+          { message: "User Profile Updated" },
+          { status: 201 }
+        );
       } else {
         return NextResponse.json(
-          { message: "User not found" },
+          { message: "User Profile not found" },
           { status: 404 }
         );
       }
@@ -101,7 +76,6 @@ export const POST = async (request: NextRequest) => {
         await existingCompany.updateOne({
           $set: {
             ...data,
-            updatedDate: Date.now(),
           },
         });
         return NextResponse.json(
@@ -125,3 +99,40 @@ export const POST = async (request: NextRequest) => {
     );
   }
 };
+
+// export const GET = async () => {
+//   console.log("Running GET Request: Get All User Details");
+
+//   try {
+//     await connectMongo();
+
+//     const role = await currentRole();
+//     const user = await currentUser();
+
+//     if (!user || !role) {
+//       return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+//     }
+
+//     if (role === "USER") {
+//       const allUsers = await UserProfile.find().sort({ joinedDate: 1 });
+//       return NextResponse.json(allUsers, { status: 200 });
+//     }
+
+//     if (role === "SERVICE_PROVIDER") {
+//       const allServiceProviders = await ServiceProviderModel.find()
+//       .sort({ joinedDate: 1 });
+//       return NextResponse.json(allServiceProviders, { status: 200 });
+//     }
+
+//     if (role === "COMPANY") {
+//       const allCompanies = await CompanyModel.find()
+//       .sort({ joinedDate: 1 });
+//       return NextResponse.json(allCompanies, { status: 200 });
+//     }
+
+//     return NextResponse.json({ message: "Not Authorized" }, { status: 401 });
+//   } catch (error) {
+//     console.error("Error fetching users info:", error);
+//     return NextResponse.json({ message: "Server error" }, { status: 500 });
+//   }
+// };
--- lib/auth.ts
+++ lib/auth.ts
@@ -3,13 +3,13 @@ import { auth } from "@/auth";
 export const currentUser = async () => {
   const session = await auth();
   //console.log("Session", session);
-  // const user = { id: "67036e63ae558d01860c125d" };
-  // return user;
-  return session?.user;
+  const user = { id: "67038b95ae6bd78826527043" };
+  return user;
+  //return session?.user;
 };
 
 export const currentRole = async () => {
   const session = await auth();
-  return "SERVICE_PROVIDER";
+  return "USER";
   //return session?.user?.role;
 };
