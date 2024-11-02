f127f83e761edf3a8fbb93b41bff7cf2d81dc41a
Manish Maharjan <maharjanm96@gmail.com>
2024-11-02 21:07:45 +0545

    MinIO configuration for file upload

---
+++ lib/constants.ts
@@ -0,0 +1,10 @@
+export const MINIOURL = "http://127.0.0.1:9000/sewaverse/";
+
+export const BUCKET_NAME = "sewaverse";
+
+export const UserRole = {
+  USER: "USER",
+  SERVICE_PROVIDER: "SERVICE_PROVIDER",
+  COMPANY: "COMPANY",
+  ADMIN: "ADMIN",
+};
