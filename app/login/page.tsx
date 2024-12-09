c696b168e4284bb47d1c3e0f32a81b01da51278e
suman shrestha <suman.shrestha@logiclync.com>
2024-12-09 11:25:45 +0545

    updated env

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -51,8 +51,8 @@ export default function LoginForm() {
         <Image
           src="/images/login.svg"
           alt="login image"
-          width={100}
-          height={100}
+          width={300}
+          height={300}
         />
       </div>
       <div>
