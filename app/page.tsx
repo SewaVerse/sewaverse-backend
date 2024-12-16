9f8b2568bd13d7bb7988c19fd3f5fa347aa52da7
Manish Maharjan <maharjanm96@gmail.com>
2024-12-16 12:25:11 +0545

    frontend route fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <div>Hello World</div>;
-}
+import { auth } from "@/auth";
+import React from "react";
+
+const Home = async () => {
+  const session = await auth();
+  return (
+    <div className="text-center font-poppins font-medium text-xl">
+      <h1>Id:{session?.user?.id}</h1>
+      <h1>Name:{session?.user?.name}</h1>
+      <h1>Email:{session?.user?.email}</h1>
+      <h1>Role:{session?.user?.roles}</h1>
+    </div>
+  );
+};
+
+export default Home;
