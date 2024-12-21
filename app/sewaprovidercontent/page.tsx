02a32d9a6da30b094e820a067127fd895843f28a
suzaan-22 <suzaanmahato22@gmail.com>
2024-12-22 01:20:13 +0545

    "Added '/sewaprovidercontent' to PUBLIC_ROUTES array"

---
+++ app/sewaprovidercontent/page.tsx
@@ -0,0 +1,32 @@
+import { Button } from "@/components/ui/button";
+import { currentUser } from "@/lib/auth";
+import Image from "next/image";
+import Link from "next/link";
+
+export default async function SewaProviderWelcomePage() {
+  const user = await currentUser();
+  return (
+    <div className="flex flex-col items-center justify-center p-10 my-10 bg-white h-full ">
+      {/* Greeting Section */}
+      <div className="flex flex-col items-center text-center px-6">
+        <h1 className=" text-2xl md:text-3xl font-semibold text-gray-800 my-4">
+          Namaste <span className="text-purple-600 capitalize">{user?.name}</span>,
+        </h1>
+        <p className="text-black text-base md:text-lg">
+          We’ve received your details and are currently reviewing to ensure a
+          safe, reliable, and trustworthy <br /> platform. You’ll hear back from us
+          soon with an update on your verification status.
+        </p>
+
+        <p className="mt-6 text-black font-medium  ">
+          <strong>In the meantime</strong>, let’s build a fantastic profile to showcase you and your amazing services.
+        </p>
+
+        {/* Redirection Notice */}
+        <p className="mt-4 text-blue-600 text-sm md:text-base italic  decoration-blue-800">
+          Redirecting you to the next step...
+        </p>
+      </div>
+    </div>
+  );
+}
