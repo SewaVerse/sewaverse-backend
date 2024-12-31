f039744539417ce03cfc35eeba0306cc3649408c
suzaan-22 <suzaanmahato22@gmail.com>
2024-12-31 17:00:34 +0545

    updated

--- app/sewaproviderstep1/page.tsx
+++ app/sewaproviderstep1/page.tsx
@@ -51,9 +51,18 @@ export default function SewaProviderStep1() {
               <option value="" disabled>
                 Experienced (Required*)
               </option>
+              <option value="freshers"> Freshers</option>
               <option value="1"> 1</option>
               <option value="2"> 2</option>
               <option value="3"> 3</option>
+              <option value="4"> 4</option>
+              <option value="5"> 5</option>
+              <option value="6"> 6</option>
+              <option value="7"> 7</option>
+              <option value="8"> 8</option>
+              <option value="9"> 9</option>
+              <option value="10"> 10</option>
+              <option value="morethan10"> More Than 10 years</option>
             </select>
           </div>
 
