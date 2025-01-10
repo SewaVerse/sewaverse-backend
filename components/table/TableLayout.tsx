0aca064cf06a4fb72972e701f2e14a6a2dd552ce
Suman Shrestha <suman.shrestha@logiclync.com>
2025-01-10 17:15:10 +0545

    feat: implement delete confirmation dialog in table layout

--- components/table/TableLayout.tsx
+++ components/table/TableLayout.tsx
@@ -1,6 +1,10 @@
 import { ColumnDef } from "@tanstack/react-table";
 import { Plus } from "lucide-react";
+import { useState } from "react";
 
+import appendActionColumn from "@/app/utils/appendActionColumn";
+
+import DeleteAlert from "../common/DeleteAlert";
 import { Button } from "../ui/button";
 import { DataTable } from "./DataTable";
 
@@ -11,16 +15,33 @@ type TableLayoutProps<T> = {
   isLoading: boolean;
   columns: ColumnDef<T>[];
   children?: React.ReactNode;
+  onDelete?: (row: T) => void;
 };
 
-const TableLayout = <T,>({
+const TableLayout = <T extends { id: string }>({
   heading,
   addText,
   data,
   isLoading,
   columns,
   children,
+  onDelete,
 }: TableLayoutProps<T>) => {
+  const [state, setState] = useState<T | null>(null);
+  const [openDeleteAlert, setDeleteAlert] = useState(false);
+
+  const handleDelete = (row: T) => {
+    setState(row);
+    setDeleteAlert(true);
+  };
+
+  const deleteConfirm = () => {
+    if (state && onDelete) {
+      onDelete(state);
+    }
+  };
+
+  const newColumns = appendActionColumn(columns, () => {}, handleDelete);
   return (
     <div className="container mx-auto p-3">
       <h1 className="text-2xl font-bold mb-2">{heading}</h1>
@@ -34,10 +55,20 @@ const TableLayout = <T,>({
       </div>
 
       <div className="mt-3">
-        <DataTable columns={columns} data={data ?? []} isLoading={isLoading} />
+        <DataTable
+          columns={newColumns}
+          data={data ?? []}
+          isLoading={isLoading}
+        />
       </div>
 
       {children}
+      <DeleteAlert
+        open={openDeleteAlert}
+        onOpenChange={setDeleteAlert}
+        onDelete={deleteConfirm}
+        setState={setState}
+      />
     </div>
   );
 };
