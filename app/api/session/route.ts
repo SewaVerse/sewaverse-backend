63caf818c03244f23c7d98d326fd609451501a1a
Manish Maharjan <maharjanm96@gmail.com>
2025-01-10 14:35:18 +0545

    feat: add userType to login response and create session API endpoint

---
+++ app/api/session/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+import { asyncHandler } from "@/app/utils/asyncHelper/asyncHandler";
+import { getcurrentUser } from "@/lib/auth";
+
+export const GET = asyncHandler(async (request: Request) => {
+  console.warn(request.url);
+
+  const user = await getcurrentUser();
+  console.warn(user);
+
+  return NextResponse.json({
+    success: true,
+    message: "User session fetched",
+    data: user,
+  });
+});
