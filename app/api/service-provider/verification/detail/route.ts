f0434eb5323009267e90d509f9b983902f74418d
Manish Maharjan <maharjanm96@gmail.com>
2025-01-10 13:16:23 +0545

    Refactor profile page layout; update file upload component to synchronize with form state

--- app/api/service-provider/verification/detail/route.ts
+++ app/api/service-provider/verification/detail/route.ts
@@ -22,7 +22,7 @@ import ApiError from "@/app/utils/apiError";
 import roleAsyncHandler from "@/app/utils/asyncHelper/roleAsyncHandler";
 import { genderTypeMap } from "@/app/utils/enumMap";
 import { validateRequestBody } from "@/app/utils/validateRequestBody";
-import { currentNextAuthUser } from "@/lib/auth";
+import { getcurrentUser } from "@/lib/auth";
 import db from "@/lib/db";
 
 function parseProviderVerificationDetail(
@@ -74,7 +74,7 @@ export const POST = roleAsyncHandler(
       return NextResponse.json(validationError, { status: 400 }); // If there's an error, return it directly
     }
 
-    const user = await currentNextAuthUser();
+    const user = await getcurrentUser();
 
     let serviceProvider = await getServiceProviderByUserId(user!.id!);
 
