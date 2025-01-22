eaf1942588e093bdd3447931fc57abf86a26afcd
Suman Shrestha <shresthasuman839@gmail.com>
2025-01-22 22:24:41 +0545

    - build issue fix

--- components/CarouselWithPreview.tsx
+++ components/CarouselWithPreview.tsx
@@ -15,13 +15,13 @@ import {
 export function CarouselDemo() {
   const [api, setApi] = useState<CarouselApi>();
   const [current, setCurrent] = useState(0);
-  const [count, setCount] = useState(0);
+  //const [count, setCount] = useState(0);
 
   useEffect(() => {
     if (!api) {
       return;
     }
-    setCount(api.scrollSnapList().length);
+    //setCount(api.scrollSnapList().length);
     setCurrent(api.selectedScrollSnap() + 1);
 
     api.on("select", () => {
