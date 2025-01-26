e22b8c79b062ebdf28ab1ea617ecacfaf195bdd7
Suman Shrestha <shresthasuman839@gmail.com>
2025-01-26 15:02:40 +0545

    feat: add utility function to retrieve image URL from Prisma file with binary data handling

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,33 @@
+import { Prisma } from "@prisma/client";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function getImageUrlFromPrismaFile(
+  file: Prisma.FileGetPayload<{ include: { fileBinaries: true } }> | null
+): string {
+  if (!file) return "";
+
+  if (!file.fileBinaries || file.fileBinaries.length === 0) {
+    return ""; // No binary data available
+  }
+
+  const binaryData = file.fileBinaries[0].data;
+  if (!binaryData) {
+    return ""; // Binary data is null
+  }
+
+  const uint8Array =
+    binaryData instanceof Uint8Array
+      ? binaryData
+      : new Uint8Array(Object.values(binaryData));
+
+  // Convert Uint8Array to Buffer
+  const bufferData = Buffer.from(uint8Array);
+
+  const base64String = bufferData.toString("base64");
+  return `data:${file.type};base64,${base64String}`;
 }
